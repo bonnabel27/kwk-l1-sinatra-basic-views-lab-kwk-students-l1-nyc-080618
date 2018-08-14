e59c8779dfc354a4d14b3621a4680c8dfcf39fb1
bonnabel27 <sika.attikesse@gmail.com>
2018-08-14 19:35:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-
-
+class App <Sinatra::Base
+  get '/' do
+    erb :index
+  end
+  
+  get '/sika' do
+    erb :sika
+  end
+  get '/sarah' do
+    erb :sarah
+  end
+  get 'kazi' do
+    erb :kazi
+  end
+  get 'april' do
+    erb :april
+  end
 end
